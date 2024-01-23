3615d7c6a6c8a97ea57fe2520841f8ffe306fb8d
Nikita Zytsev <eronmcgonagal@gmail.com>
2024-01-23 11:55:44 +0300

    commit

--- uni/src/components/Login.jsx
+++ uni/src/components/Login.jsx
@@ -6,7 +6,7 @@ const Login = () => {
   return (
     <div className="second h-[100vh] flex items-center justify-center">
       <div className="blockInf rounded-2xl py-2 px-2 ml:px-10">
-        <h2 className="text-primary text-3xl font-jura font-bold">Sign in</h2>
+        <h2 className="text-primary text-3xl font-jura font-normal">Sign in</h2>
         <div className="flex flex-col gap-4 pt-7 ml:items-center">
           <div className="flex justify-between ml:gap-16 flex-col ml:flex-row">
             <span className="text-white text-2xl font-juru font-[400]">
@@ -16,7 +16,7 @@ const Login = () => {
               type="email"
               name="email"
               id="email"
-              className=" bg-transparent outline-none border-[1px] border-line text-white text-xl rounded-[10px]"
+              className=" bg-transparent outline-none border-[1px] border-line text-white text-xl rounded-[10px] font-gil"
               placeholder="Email"
             />
           </div>
@@ -28,16 +28,13 @@ const Login = () => {
               type="password"
               name="password"
               id="password"
-              className=" bg-transparent outline-none border-[1px] border-line text-white text-xl rounded-[10px]"
+              className=" bg-transparent outline-none border-[1px] border-line text-white text-xl rounded-[10px] font-gil"
               placeholder="Password"
             />
           </div>
-          <span className="text-primary text-3xl font-jura font-bold text-center cursor-pointer">
-            Forget password?
-          </span>
           <Link
             to="/main"
-            className="text-white font-jura font-bold flex items-center justify-center w-[240px] py-3 border-[1px] border-line rounded-full mt-4"
+            className="text-white text-xl font-gilSemi flex items-center justify-center w-[240px] py-3 border-[1px] border-line rounded-full mt-4"
           >
             Sign in
           </Link>
